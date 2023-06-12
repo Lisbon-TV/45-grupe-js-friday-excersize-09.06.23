4082b0a589434d6763b510d66e5cd166337933d6
Lisbon <lisbon@dublin.com>
2023-06-12 08:14:45 +0300

    Excercises

--- js/01-console.js
+++ js/01-console.js
@@ -1,1 +1,31 @@
 console.clear();
+
+
+// 1) Naršyklėje nupieškite linija iš 400 “*”. 
+// Programiškai “suskaldykite” žvaigždutes taip, 
+// kad vienoje eilutėje nebūtų daugiau nei 50 “*”.
+
+
+// 2) Sugeneruokite 300 atsitiktinių skaičių nuo 0 iki 300, 
+// atspausdinkite juos atskirtus tarpais ir suskaičiuokite
+// kiek tarp jų yra didesnių už 150.  
+// Skaičiai didesni nei 275 turi būti raudonos spalvos.
+
+
+// 3) Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000,
+// kurie dalijasi iš 77 be liekanos. Skaičius atskirkite kableliais. 
+// Po paskutinio skaičiaus kablelio neturi būti. 
+// Jeigu reikia, panaudokite css, kad visi rezultatai matytusi ekrane.
+
+
+// 4) Nupieškite kvadratą iš “*”, kurio kraštines sudaro 100 “*”. Panaudokite css stilių, kad kvadratas ekrane atrodytų kvadratinis.
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+
+
+// 5) Prieš tai nupieštam kvadratui nupieškite raudonas istrižaines.
